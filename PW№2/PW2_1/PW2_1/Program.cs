31bcd63c68f8a75529aea39de70cfd7d1e5e2099
NeonRider132 <123200771+NeonRider132@users.noreply.github.com>
2023-03-26 22:27:44 +0300

    Add files via upload

---
+++ PW№2/PW2_1/PW2_1/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace PW2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            equa equation = new equa();
+            Console.WriteLine(equation.VarA());
+            Console.WriteLine(equation.VarB());
+            Console.WriteLine(equation.VarC());
+            equation.CalculateRoots();
+        }
+    }
+    public class equa
+    {
+        private double a = -1;
+        private double b = 2;
+        private double c = 3;
+        public double VarA()
+        {
+            return a;
+        }
+        public double VarB()
+        {
+            return b;
+        }
+        public double VarC()
+        {
+            return c;
+        }
+        public void SetA(double a)
+        {
+            this.a = a;
+        }
+        public void SetB(double b)
+        {
+            this.b = b;
+        }
+        public void SetC(double c)
+        {
+            this.c = c;
+        }
+        private double discrim()
+        {
+            return (VarB() * VarB()) - 4 * VarA() * VarC();
+        }
+        public void CalculateRoots()
+        {
+            if (discrim() > 0 || discrim() == 0)
+            {
+                double x1 = (-VarB() + Math.Sqrt(Convert.ToInt32(discrim()))) / (2 * VarA());
+                double x2 = (-VarB() - Math.Sqrt(Convert.ToInt32(discrim()))) / (2 * VarA());
+                Console.WriteLine("x1= {0}   x2= {1}", x1, x2);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Действительных корней нет");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ PW№2/PW2_2/PW2_2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace PW2_2
+{
+    class programm
+    {
+        static void Main()
+        {
+            replic number = new replic();
+            for (int i = 0; i < number.Getn(); i++)
+            {
+                number.SetNumber(Convert.ToInt32(Console.ReadLine()));
+            }
+        }
+    }
+    class replic
+    {
+        private int n = 0;
+        public void SetNumber(int number)
+        {
+            if (number == n + 1)
+            {
+                Console.WriteLine(Convert.ToInt32(Getn()));
+                n = number;
+            }
+            else
+            {
+                Console.WriteLine(Convert.ToInt32(Getn()));
+                Console.WriteLine("error");
+                n = 0;
+            }
+        }
+        public bool Ret()
+        {
+            return Ret();
+        }
+        public int Getn()
+        {
+            return n + 1;
+        }
+    }
+
+}
